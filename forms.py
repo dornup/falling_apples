0ed89a137fb351d9424a3d117391b5cc4cfc31d4
Admin <mdiscool1@mail.ru>
2024-07-03 17:59:26 +0700

    r

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length 
 
 class RegisterForm(FlaskForm):
     login = StringField(label='Логин', validators=[
